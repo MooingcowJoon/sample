6a6e23a6dcd101b283417b2b2676d69d8558dee4
Joonho Seo <joonhoseo1991@gmail.com>
2023-04-12 10:11:54 +0900

    rererere

--- src/sample/sample2.java
+++ src/sample/sample2.java
@@ -2,7 +2,7 @@ package sample;
 
 public class sample2 {
 	public static void main(String[] args) {
-		System.out.println("ddddddd");
+		System.out.println("dddddddddd");
 		System.out.println("hed");
 	}
 }
